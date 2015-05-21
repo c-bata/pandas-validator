9742d9c753959849999e9a1f6159c59d504241af
c-bata <contact@c-bata.link>
2015-05-21 23:11:13 +0900

    refs #4 Fix typo

--- pandas_validator/validators/columns.py
+++ pandas_validator/validators/columns.py
@@ -1,10 +1,10 @@
 from .series import *
 
 
-class IntegerColumnVaridator(IntegerSeriesValidator):
+class IntegerColumnValidator(IntegerSeriesValidator):
     def __init__(self, label, *args, **kwargs):
-        super(IntegerColumnVaridator, self).__init__(*args, **kwargs)
+        super(IntegerColumnValidator, self).__init__(*args, **kwargs)
         self.label = label
 
     def is_valid(self, dataframe):
-        return super(IntegerColumnVaridator, self).is_valid(dataframe[self.label])
+        return super(IntegerColumnValidator, self).is_valid(dataframe[self.label])
