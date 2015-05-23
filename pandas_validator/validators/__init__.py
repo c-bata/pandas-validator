28a7a3151ad58e5f41ae2c2c16e0370c9ff08fe5
c-bata <contact@c-bata.link>
2015-05-24 00:00:30 +0900

    Change the namespaces

--- pandas_validator/validators/__init__.py
+++ pandas_validator/validators/__init__.py
@@ -1,3 +1,12 @@
-from pandas_validator.validators.series import *
-from pandas_validator.validators.columns import *
-from pandas_validator.validators.dataframe import *
+from pandas_validator.validators.series import (
+    BaseSeriesValidator,
+    IntegerSeriesValidator,
+    FloatSeriesValidator,
+)
+from pandas_validator.validators.columns import (
+    IntegerColumnValidator,
+    FloatColumnValidator,
+)
+from pandas_validator.validators.dataframe import (
+    DataFrameValidator,
+)
