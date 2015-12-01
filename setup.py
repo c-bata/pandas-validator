a9fd9e763c2dd2b63027147108ef00f0a16a5c49
c-bata <contact@c-bata.link>
2015-12-01 15:28:28 +0900

    Hot fix: Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,43 @@
 import os
-from setuptools import setup
-
-__author__ = 'Masashi Shibata <contact@c-bata.link>'
-__version__ = '0.3.2'
+from setuptools import setup, find_packages
+from pandas_validator import (
+    __version__,
+    __license__,
+    __author__,
+    __author_email__,
+)
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(BASE_PATH, 'README.rst')).read()
 CHANGES = open(os.path.join(BASE_PATH, 'CHANGES.rst')).read()
 
+__classifiers__ = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+]
+
 setup(
     name='pandas_validator',
     version=__version__,
     author=__author__,
-    author_email='contact@c-bata.link',
+    author_email=__author_email__,
     url='https://github.com/c-bata/pandas-validator',
     description='Validate the pandas objects such as DataFrame and Series.',
     long_description=README + '\n\n' + CHANGES,
-    package=['pandas_validator'],
+    packages=find_packages(exclude=['test*']),
     install_requirements=['pandas'],
-    keywords=['pandas', 'validator'],
-    license='MIT License',
+    keywords='pandas validator',
+    license=__license__,
     include_package_data=True,
     test_suite='pandas_validator',
 )
